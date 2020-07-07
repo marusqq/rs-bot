ffc94e87c2db1e38d858ed81e69ea4c378f1201f
Marius Pozniakovas <pozniakovui@gmail.com>
2020-07-08 01:33:12 +0300

    Initial bot commit

---
+++ bot.py
@@ -0,0 +1,17 @@
+_author_ = "Marius"
+'''rs bot main script'''
+
+import cv2
+import numpy as np
+
+img_rgb = cv2.imread('full_photo.png')
+template = cv2.imread('rocks.png')
+w, h = template.shape[:-1]
+
+res = cv2.matchTemplate(img_rgb, template, cv2.TM_CCOEFF_NORMED)
+threshold = .8
+loc = np.where(res >= threshold)
+for pt in zip(*loc[::-1]):  # Switch collumns and rows
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+
+cv2.imwrite('result.png', img_rgb)
